28aecddfad3529b8bf4d46dfda743ea32ec402fd
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2024-06-24 19:46:55 +0200

    update prod script add notes about restart policy

--- scripts/docker/prod.sh
+++ scripts/docker/prod.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 docker build --target deploy -t livo:deploy .
+# remove container if it already exists
+docker container rm --force livo-prod
+# NOTE: restart policy needs to be "always"
+# otherwise, restart will not take effect after reboot...
+# for more information, see:
+# https://docs.docker.com/config/containers/start-containers-automatically/
 docker run \
 --privileged \
 --network host \
 -v /dev:/dev \
 --name livo-prod \
---restart unless-stopped \
+--restart always \
 livo:deploy
