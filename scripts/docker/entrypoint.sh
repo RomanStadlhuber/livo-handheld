129ec1f23adae57a2a7730f547305cb3510f8b8d
RomanStadlhuber <roman.stadlhuber@gmail.com>
2025-07-01 23:01:36 +0200

    update entrypoint set number of workers to 1 and add note about  multi-worker problems this fixes the different behavior of the prod server relaitve to the dev server

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -2,4 +2,7 @@
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /ros2_ws/install/setup.bash
 cd /app
-gunicorn -w 4 -b 0.0.0.0:8000 app:app
+# IMPORTANT: the web application CANNOT handle multiple workers,
+# since references to the ROS2 launch processes are handled as globals
+# therefore, the number of workers MUST be set to 1
+gunicorn -w 1 -b 0.0.0.0:8000 app:app
